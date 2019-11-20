19d221c38e4687b568d34d64580b71f50e8204d7
Yummy <ujjang@trinitysoft.co.kr>
2019-11-20 14:24:51 +0900

    add to coroutine and exception

---
+++ coroutine.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+#define crBegin static int state=0; \
+	switch(state) { \
+	case 0:
+		#define crReturn(i, x) do { \
+			state = i; \
+			return x; \
+			case i:; \
+	} while (0)
+	#define crFinish \
+}
+
+int loop() {
+	static int i;
+	crBegin;
+	for(i = 1; i < 10; i++)
+		crReturn(1, i);
+	crFinish;
+}
+
+int main() {
+	int i;
+
+	for(;i = loop();) 
+		if(i > 100) break;
+		else printf("%d\n", i); 
+
+	return 0;
+}
